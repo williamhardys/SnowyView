72e8d227fecbb80c9d923a8a7af252896a5f88c5
Rawand Ahmed <rawandahmad698@gmail.com>
2017-12-25 20:48:22 +0300

    Initial Commit

---
+++ SnowyView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnowyView
+//
+//  Created by Rawand Ahmed Shaswar on 12/25/17.
+//  Copyright © 2017 DC Middle East. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
