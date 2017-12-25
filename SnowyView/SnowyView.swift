eef9bb7d4e357f3069a5b32aa576a04281fa6dfa
Rawand Ahmed <rawandahmad698@gmail.com>
2017-12-25 21:36:34 +0300

    _

---
+++ SnowyView/SnowyView.swift
@@ -0,0 +1,65 @@
+
+//
+//  MyView.swift
+//  Practice
+//
+//  Created by Rawand Ahmed Shaswar on 12/25/17.
+//  Copyright © 2017 DC Middle East. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+var timer = Timer()
+var started : Bool = false
+
+
+
+extension UIView {
+    
+   
+    @objc func AddSnow(Interval: TimeInterval) {
+        if started == true {
+           // Hi...!
+            
+        } else {
+            scheduledTimerWithTimeInterval(Intv: Interval)
+        }
+
+        self.addSubview(AddEffect(x: CGFloat(arc4random() % UInt32(self.bounds.width)), y:  self.frame.minY - 5))
+
+    }
+    
+    private func moveSnow (WithView View:UIView) {
+        UIView.animate(withDuration: 16, animations: {
+            let RandomX = CGFloat(arc4random() % UInt32(self.bounds.width))
+            View.frame.origin = CGPoint(x: RandomX, y:self.frame.maxY + 20)
+        })
+     
+    }
+    
+    
+    private func AddEffect (x:CGFloat,y:CGFloat) -> UIView {
+        let RandomS = arc4random() % 10
+
+        let snow = UIView(frame: CGRect(x: x, y: y, width:  CGFloat(RandomS), height: CGFloat(RandomS)))
+        snow.layer.cornerRadius = snow.frame.size.width / 2
+        snow.clipsToBounds = true
+        snow.backgroundColor = .white
+        moveSnow(WithView: snow)
+
+        
+        return snow
+    }
+    
+    func scheduledTimerWithTimeInterval(Intv : TimeInterval){
+        
+        if Intv == 0 {
+           fatalError("Invalid Interval,... 0.5 is the default TimeInterval")
+        }
+        started = true
+        timer = Timer.scheduledTimer(timeInterval: Intv, target: self, selector: #selector(self.AddSnow), userInfo: nil, repeats: true)
+    }
+    
+
+}
--- SnowyView/ViewController.swift
+++ SnowyView/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.AddSnow(Interval: 0.5)
+        
     }
 
     override func didReceiveMemoryWarning() {
